0bef7730bd5e54e9b2a81a6406dcd075a17fe5b4
Lorena Oswald <oswaldl@student.ethz.ch>
2020-06-04 18:01:57 +0200

    completed costfunctions still missing unique-paths implementation

--- src/agents/CentralizedAgent.java
+++ src/agents/CentralizedAgent.java
@@ -5,26 +5,24 @@ import simEngine.EdgeCosts;
 import simEngine.NetworkCostGraph;
 import simEngine.SimConfig;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 
 public class CentralizedAgent implements NetworkAgent {
-    HashMap<Integer, LinkedList<Integer>> solutions;
-    private int totalAgents;
-    private HashMap<Integer[], Float> distributionCost = new HashMap<>();
-    private ArrayList<LinkedList<Integer>> uniquePaths;
-    private int n;
-
+    static HashMap<Integer, LinkedList<Integer>> solutions;
+    static private int totAgents;
+    static private EdgeCosts ec;
+    static private HashMap<Integer[], Double> distributionCost = new HashMap<>();
+    static private ArrayList<LinkedList<Integer>> uniquePaths;
+    static private int n;
 
     @Override
-    public LinkedList<Integer> agentDecide(NetworkCostGraph ncg, EdgeCosts ec, int decidedAgents, int totalAgents) {
+    public LinkedList<Integer> agentDecide(NetworkCostGraph ncg, EdgeCosts ec1, int decidedAgents, int totalAgents) {
         // either calculate solution and use it or just use pre-calculated solution
         if (decidedAgents == 0 || solutions.isEmpty()) {
-            // need a way to get access to total amount of agents by either passing SimConfig or just number to agentDecide
+            totAgents= totalAgents;
+            ec = ec1;
             solutions = new HashMap<>(totalAgents);
-            solutions = completeSolution(ncg, ec);
+            solutions = completeSolution(ncg);
         }
         return solutions.get(decidedAgents);
     }
@@ -33,25 +31,24 @@ public class CentralizedAgent implements NetworkAgent {
      * This will calculate the solution for all agents by finding the optimal paths and corresponding loads.
      *
      * @param ncg the adj. matrix of the current latency cost for the network to decide upon
-     * @param ec  an object to figure out how much an edge costs, given different amounts of agents on it
      * @return Map of agent nr. to an optimal path. Multiple agents may refer to same path.
      */
-    public HashMap<Integer, LinkedList<Integer>> completeSolution(NetworkCostGraph ncg, EdgeCosts ec) {
+    public HashMap<Integer, LinkedList<Integer>> completeSolution(NetworkCostGraph ncg) {
         uniquePaths = getUniquePaths(ncg);
         n = uniquePaths.size();
 
         // calculate all distribution costs
-        recDistributionCost(0, new Integer[n], totalAgents);
+        recDistributionCost(0, new Integer[n], totAgents);
 
         // find minimal value of all distribution costs
-        float min = Integer.MAX_VALUE;
-        for (Map.Entry<Integer[], Float> current : distributionCost.entrySet()) {
+        double min = Integer.MAX_VALUE;
+        for (Map.Entry<Integer[], Double> current : distributionCost.entrySet()) {
             min = Math.min(min, current.getValue());
         }
 
         // save all distributions that create a minimal value in list
         ArrayList<Integer[]> bestDistributions = new ArrayList<>();
-        for (Map.Entry<Integer[], Float> current : distributionCost.entrySet()) {
+        for (Map.Entry<Integer[], Double> current : distributionCost.entrySet()) {
             if (current.getValue() == min) {
                 bestDistributions.add(current.getKey());
             }
@@ -61,7 +58,7 @@ public class CentralizedAgent implements NetworkAgent {
         // for now just choosing first one
         Integer[] bestDistribution = bestDistributions.get(0);
 
-        HashMap<Integer, LinkedList<Integer>> solutionsPerAgent = new HashMap<>(totalAgents);
+        HashMap<Integer, LinkedList<Integer>> solutionsPerAgent = new HashMap<>(totAgents);
         int agentsum = 0;
         for (int i = 0; i < bestDistribution.length; i++) {
             int agents = bestDistribution[i];
@@ -104,11 +101,28 @@ public class CentralizedAgent implements NetworkAgent {
      * Calculate cost of a given path.
      *
      * @param path path as a list of nodes
-     * @param ec an object to figure out how much an edge costs, given different amounts of agents on it
      * @return Cost as a real value.
      */
-    public Float calcPathCost(LinkedList<Integer> path, EdgeCosts ec) {
-        return null;
+    public double calcPathCost(LinkedList<Integer> path) {
+        double pathCost = 0;
+        // empty path or only 1 node
+        if (path.isEmpty() || path.size() < 2) {
+            return 0.0;
+        }
+
+        // path of at least length 1
+
+        Iterator<Integer> it = path.listIterator();
+
+        int current = it.next();
+        int next = it.next();
+        while (it.hasNext()) {
+            pathCost += ec.getEdgeCost(current, next);
+            current = next;
+            next = it.next();
+        }
+        pathCost += ec.getEdgeCost(current, next);
+        return pathCost;
     }
 
     /**
@@ -117,7 +131,11 @@ public class CentralizedAgent implements NetworkAgent {
      * @param distribution distribution of agents over unique paths.
      * @return Cost to route agents according to distribution over paths
      */
-    public Float calcDistributionCost(Integer[] distribution) {
-        return null;
+    public Double calcDistributionCost(Integer[] distribution) {
+        double distributionSum = 0;
+        for (int i = 0; i < uniquePaths.size(); i++) {
+            distributionSum += distribution[i] * calcPathCost(uniquePaths.get(i));
+        }
+        return (1.0/totAgents*distributionSum);
     }
 }
