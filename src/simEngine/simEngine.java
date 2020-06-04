db8f77dd6077cebe3b77e50bb719a95991953e2f
Tobias <Toroto006@hotmail.de>
2020-06-04 14:12:13 +0200

    Added modular agentsPerSimulationStep

--- src/simEngine/simEngine.java
+++ src/simEngine/simEngine.java
@@ -76,8 +76,9 @@ public class simEngine {
         }
         // Read the rest of the config
         int amountOfAgents = jsonObject.getInt("amountOfAgents");
+        int agentsPerStep = jsonObject.getInt("agentsPerStep");
         String networkTitle = jsonObject.getString("networkTitle");
-        return new SimConfig(nodes, networkGraph, amountOfAgents, networkTitle);
+        return new SimConfig(nodes, networkGraph, amountOfAgents, networkTitle, agentsPerStep);
     }
 
     private static HashMap<Integer, String> createNodeMappingIS(String[] nodes) {
@@ -106,7 +107,6 @@ public class simEngine {
      * This will initialise the JSONObject of one Simulation
      * 
      * @param simConfig
-     * @param ncgDone
      * @param nameOfAgent
      * @param out
      */
@@ -193,7 +193,7 @@ public class simEngine {
             networkCostGraph.calculateAllCosts();
 
             
-            if(doneAgents % 100 == 0 || doneAgents == simConfig.getAmountOfAgents() - 1) {
+            if(doneAgents % simConfig.getAgentsPerStep() == 0 || doneAgents == simConfig.getAmountOfAgents() - 1) {
                 Map<String, Edge> mapEdges = new TreeMap<String, Edge>(simConfig.getNetworkGraph().getEdges());
                 int[] arr = new int[mapEdges.size()];
                 int i = 0;
