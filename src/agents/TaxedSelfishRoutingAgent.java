8c644483971235616f74f00eab40368a0cbe3063
Markus Pobitzer <markuspobitzer@gmail.com>
2020-06-04 18:42:21 +0200

    First (wrong!) implementation of taxes fct

--- src/agents/TaxedSelfishRoutingAgent.java
+++ src/agents/TaxedSelfishRoutingAgent.java
@@ -6,15 +6,13 @@ import java.util.LinkedList;
 
 public class TaxedSelfishRoutingAgent implements NetworkAgent {
 
-    private NetworkCostGraph ncg;
     private  double cost [];
     private int predecessor[];
 
     @Override
     public LinkedList<Integer> agentDecide(NetworkCostGraph ncg, EdgeCosts ec, int decidedAgents, int totalAgents) {
-        this.ncg = ncg;
         LinkedList<Integer> ret = new LinkedList<>();
-        int last = this.ncg.getNumVertices() - 1;
+        int last = ncg.getNumVertices() - 1;
 
         /* -- initialization -- */
         cost = new double[last + 1];
@@ -44,13 +42,13 @@ public class TaxedSelfishRoutingAgent implements NetworkAgent {
             for (int v:nodesLeft
             ) {
                 if(ec.contains(u, v)){
-                    updateCost(u, v, ncg);
+                    updateCost(u, v, ec, decidedAgents);
                 }
             }
         }
 
         /* -- Backtracking -- */
-        int u = this.ncg.getNumVertices() - 1;
+        int u = ncg.getNumVertices() - 1;
         ret.add(u);
         while(predecessor[u] >= 0){
             u = predecessor[u];
@@ -59,18 +57,10 @@ public class TaxedSelfishRoutingAgent implements NetworkAgent {
         return ret;
     }
 
-    //Both update cost work now!
-    private void updateCost(int u, int v, EdgeCosts ec){
-        double costs = cost[u] + ec.getEdgeCost(u, v);
-        if(costs < cost[v]){
-            cost[v] = costs;
-            predecessor[v] = u;
-        }
-    }
-
-    //TODO update cost fct
-    private void updateCost(int u, int v, NetworkCostGraph ncg){
-        double costs = cost[u] + ncg.getLatencyCost(u, v); //TODO + tax
+//TODO implement correct way of calculating taxes
+    private void updateCost(int u, int v, EdgeCosts ec, int decidedAgents){
+        //double costs = cost[u] + ec.getEdgeCost(u, v) + ec.getEdgeCost(u, v) - ec.getEdgeCostCustomAgents(u, v, decidedAgents);
+        double costs = cost[u] + ec.getEdgeCost(u, v) + decidedAgents * (ec.getEdgeCost(u, v) - ec.getEdgeCostCustomAgents(u, v, decidedAgents));
         if(costs < cost[v]){
             cost[v] = costs;
             predecessor[v] = u;
