076b4d27ad9b6a74305771a515004fd6946e99c8
anttech-code <60758064+anttech-code@users.noreply.github.com>
2020-06-12 10:57:34 +0200

    Added Solver Lib, add to ide structure

--- src/agents/TaxedSelfishRoutingAgent.java
+++ src/agents/TaxedSelfishRoutingAgent.java
@@ -1,27 +1,24 @@
 package agents;
 
+
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.variables.IntVar;
 import simEngine.EdgeCosts;
 import simEngine.LinearFct;
 import simEngine.NetworkCostGraph;
 
 import java.util.LinkedList;
 
+import static java.lang.System.exit;
+
+
 public class TaxedSelfishRoutingAgent implements NetworkAgent {
 
     private double cost[];
     private int predecessor[];
     private LinearFct fct;
-    private double factor;
-    private String name;
-
-    public TaxedSelfishRoutingAgent(LinearFct fct, String name) {
-        this.fct = fct;
-        this.name = name;
-    }
-    public TaxedSelfishRoutingAgent() {
-        this.fct = new LinearFct(0f, 1f);
-        this.name = "TaxedSelfishRoutingAgent";
-    }
+    private String name =  "TaxedSelfishRoutingAgent";
 
 
     @Override
@@ -29,10 +26,32 @@ public class TaxedSelfishRoutingAgent implements NetworkAgent {
         return this.name;
     }
 
+
+    public void test() {
+        int n = 8;
+        Model model = new Model(n + "-queens problem");
+        IntVar[] vars = new IntVar[n];
+        for(int q = 0; q < n; q++){
+            vars[q] = model.intVar("Q_"+q, 1, n);
+        }
+        for(int i  = 0; i < n-1; i++){
+            for(int j = i + 1; j < n; j++){
+                model.arithm(vars[i], "!=",vars[j]).post();
+                model.arithm(vars[i], "!=", vars[j], "-", j - i).post();
+                model.arithm(vars[i], "!=", vars[j], "+", j - i).post();
+            }
+        }
+        Solution solution = model.getSolver().findSolution();
+        if(solution != null){
+            System.out.println(solution.toString());
+        }
+    }
+
     @Override
     public LinkedList<Integer> agentDecide(NetworkCostGraph ncg, EdgeCosts ec, int decidedAgents, int totalAgents) {
 
-        factor = fct.getCost(1.0 / totalAgents * decidedAgents);
+        test();
+        exit(1);
         LinkedList<Integer> ret = new LinkedList<>();
         int last = ncg.getNumVertices() - 1;
 
@@ -80,7 +99,7 @@ public class TaxedSelfishRoutingAgent implements NetworkAgent {
     }
 
     private void updateCost(int u, int v, EdgeCosts ec) {
-        double costs = cost[u] + (ec.getEdgeCostCustomAgents(u, v, ec.getAgentsOnEdge(u, v) + 1) + ec.getAgentsOnEdge(u, v) * ec.getDerivativeEdgeCost(u, v)) * factor;
+        double costs = cost[u] + (ec.getEdgeCostCustomAgents(u, v, ec.getAgentsOnEdge(u, v) + 1) + ec.getAgentsOnEdge(u, v) * ec.getDerivativeEdgeCost(u, v));
         if (costs < cost[v]) {
             cost[v] = costs;
             predecessor[v] = u;
--- src/simEngine/SimEngine.java
+++ src/simEngine/SimEngine.java
@@ -218,13 +218,14 @@ public class SimEngine {
     public static void main(String[] args) throws Exception {
         String SimulationName = "Simulation";
         //String[] networks = {"BraessParadoxFast1", "BraessParadoxSlow1", "Pigou", "BraessParadoxFast2", "BraessParadoxSlow2"};
-        //String[] networks = {"BraessParadoxFast1", "BraessParadoxSlow1-original", "Pigou"};
-        String[] networks = {"BraessParadoxSlow1-original"};
+        String[] networks = {"BraessParadoxSlow1"};
+        //String[] networks = {"BraessParadoxSlow1-original"};
         NetworkAgent[] agents = {
                 //new SelfishRoutingAgent(),
-                //new TaxedSelfishRoutingAgent(),
+                new TaxedSelfishRoutingAgent()
+                // ,
                 //new TaxedSelfishRoutingAgent(new LinearFct(2f, 1f), "TaxedClassSelfishRoutingAgent"),
-                new CentralizedAgent()
+                //new CentralizedAgent()
         };
         System.out.println("GameTheory simEngine.simEngine started!\n");
 
